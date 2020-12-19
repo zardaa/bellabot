a3e09a42c8bb71322a2992993e1172f9f11160fa
zardaa <75716772+zardaa@users.noreply.github.com>
2020-12-19 22:11:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const qrcode = require("qrcode-terminal")
 const moment = require("moment-timezone")
 const fs = require("fs")
+const axios = require('axios')
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
             + 'FN:Ridwan\n' // full name
@@ -917,11 +918,11 @@ case 'filmanime':
             if (isMedia && type === 'image') {
                 const mediaData = await decryptMedia(message, uaOverride)
                 const imageBase64 = `data:${mimetype};base64,${mediaData.toString('base64')}`
-                await client.sendImageAsSticker(from, imageBase64)
+                await client.sendImageAsStiker(from, imageBase64)
             } else if (quotedMsg && quotedMsg.type == 'image') {
                 const mediaData = await decryptMedia(quotedMsg, uaOverride)
                 const imageBase64 = `data:${quotedMsg.mimetype};base64,${mediaData.toString('base64')}`
-                await client.sendImageAsSticker(from, imageBase64)
+                await client.sendImageAsStiker(from, imageBase64)
             } else if (args.length === 2) {
                 const url = args[1]
                 if (url.match(isUrl)) {
