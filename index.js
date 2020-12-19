548eb6b12029842645a2c67b87dff6abda812da0
zardaa <75716772+zardaa@users.noreply.github.com>
2020-12-19 19:35:29 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const qrcode = require("qrcode-terminal")
 const moment = require("moment-timezone")
 const fs = require("fs")
 const donate = require("./lib/donate.js");
+const axios = require("axios");
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
             + 'FN:Ridwan\n' // full name
