73cb058775df266f041c99716a946b17ce1dad5a
zardaa <75716772+zardaa@users.noreply.github.com>
2020-12-20 10:05:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const qrcode = require("qrcode-terminal")
-const moment = require("moment-timezone")
-const fs = require("fs")
-const axios = require('axios')
+const qrcode = require("qrcode-terminal");
+const moment = require("moment");
+const cheerio = require("cheerio");
+const get = require('got')
+const fs = require("fs");
+const dl = require("./lib/downloadImage.js");
+const fetch = require('node-fetch');
+const urlencode = require("urlencode");
+const axios = require("axios");
+const imageToBase64 = require('image-to-base64');
+const xp = require("./lib/xp.js");
+const donate = require("./lib/donate.js");
+const info = require("./lib/info.js");
+const readTextInImage = require('./lib/ocr')
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
             + 'FN:Ridwan\n' // full name
@@ -25,6 +35,199 @@ const config = {
 }
 
 const { zardaa, tanggal, waktu, instagram, whatsapp, youtube, nomer, aktif, ontime } = config
+
+else if (text == 'assalamualaikum'){
+conn.sendMessage(id, '3aalaikumsalam' ,MessageType.text);
+}
+else if (text == 'salam'){
+conn.sendMessage(id, 'Waalaikumsalam' ,MessageType.text);
+}
+else if (text == 'asalamualaikum'){
+conn.sendMessage(id, 'Waalaikumsalam' ,MessageType.text);
+}
+else if (text == 'Assalamualaikum'){
+conn.sendMessage(id, 'Waalaikumsalam' ,MessageType.text);
+}
+else if (text == 'p'){
+conn.sendMessage(id, 'Ya?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'P'){
+conn.sendMessage(id, 'Ya?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'Halo'){
+conn.sendMessage(id, 'hai' ,MessageType.text);
+}
+else if (text == 'Asu'){
+conn.sendMessage(id, 'tobat goblok' ,MessageType.text);
+}
+else if (text == 'asu'){
+conn.sendMessage(id, 'tobat goblok' ,MessageType.text);
+}
+else if (text == '.owner'){
+conn.sendMessage(id, 'Owner wa.me/+6282397075810' ,MessageType.text);
+}
+else if (text == '.Owner'){
+conn.sendMessage(id, 'Owner wa.me/+6282397075810' ,MessageType.text);
+}
+else if (text == '.ping'){
+conn.sendMessage(id, 'Speed 283.847_second_' ,MessageType.text);
+}
+else if (text == '.Ping'){
+conn.sendMessage(id, 'Speed 283.847_second_' ,MessageType.text);
+}
+else if (text == 'bangsat'){
+conn.sendMessage(id, 'tobat goblok' ,MessageType.text);
+}
+else if (text == 'Bangsat'){
+conn.sendMessage(id, 'tobat goblok' ,MessageType.text);
+}
+else if (text == 'Ngentod'){
+conn.sendMessage(id, 'tobat goblok?' ,MessageType.text);
+	}
+else if (text == 'ngentod'){
+conn.sendMessage(id, 'tobat goblok?' ,MessageType.text);
+}
+else if (text == 'Anjing'){
+conn.sendMessage(id, 'tobat goblok' ,MessageType.text);
+}
+else if (text == 'anjing'){
+conn.sendMessage(id, 'tobat goblok' ,MessageType.text);
+}
+else if (text == 'Bacot'){
+conn.sendMessage(id, 'sadar goblok' ,MessageType.text);
+}
+else if (text == 'Test'){
+conn.sendMessage(id, 'Test 1,2,3' ,MessageType.text);
+}
+else if (text == 'test'){
+conn.sendMessage(id, 'Test 1,2,3' ,MessageType.text);
+}
+else if (text == 'Hai'){
+conn.sendMessage(id, 'hai juga' ,MessageType.text);
+}
+else if (text == 'hai'){
+conn.sendMessage(id, 'hai juga' ,MessageType.text);
+}
+else if (text == '.ttp'){
+conn.sendMessage(id, ' *COMMAND PREMIUM CHAT WA https://wa.me/6289655478810* ' ,MessageType.text);
+}
+else if (text == 'Woi'){
+conn.sendMessage(id, 'Ya?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'Eoy'){
+conn.sendMessage(id, 'Ya?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'woi'){
+conn.sendMessage(id, 'Ya?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'eoy'){
+conn.sendMessage(id, 'Ya?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'Hi'){
+conn.sendMessage(id, 'hai juga' ,MessageType.text);
+}
+else if (text == 'hi'){
+conn.sendMessage(id, 'hai juga' ,MessageType.text);
+}
+else if (text == 'Gan'){
+conn.sendMessage(id, 'kenapa?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'Sis'){
+conn.sendMessage(id, 'kenapa?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'Bro'){
+conn.sendMessage(id, 'kenapa?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'Min'){
+conn.sendMessage(id, 'kenapa?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'Sayang'){
+conn.sendMessage(id, 'kamu bukan pacarku' ,MessageType.text);
+}
+else if (text == 'I love u'){
+conn.sendMessage(id, 'najis' ,MessageType.text);
+}
+else if (text == 'Mas'){
+conn.sendMessage(id, 'Ya?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'Mba'){
+conn.sendMessage(id, 'Ya?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'Bre'){
+conn.sendMessage(id, 'Ya?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'Cuy'){
+conn.sendMessage(id, 'Ya?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'gan'){
+conn.sendMessage(id, 'kenapa?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'sis'){
+conn.sendMessage(id, 'kenapa?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'bro'){
+conn.sendMessage(id, 'kenapa?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'min'){
+conn.sendMessage(id, 'kenapa?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'sayang'){
+conn.sendMessage(id, 'kamu bukan pacarku' ,MessageType.text);
+}
+else if (text == 'i love u'){
+conn.sendMessage(id, 'najis' ,MessageType.text);
+}
+else if (text == 'mas'){
+conn.sendMessage(id, 'Ya?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'mba'){
+conn.sendMessage(id, 'Ya?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'bre'){
+conn.sendMessage(id, 'Ya?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'cuy'){
+conn.sendMessage(id, 'Ya?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == '.frendsowner'){
+conn.sendMessage(id, 'kawan1 iskandar wa.me/+628165466368' ,MessageType.text);
+}
+else if (text == '.coowner'){
+conn.sendMessage(id, 'kawan2 testing script wa.me/+628165466368' ,MessageType.text);
+}
+else if (text == '.groupbot'){
+conn.sendMessage(id, 'group bot whatsapp https://chat.whatsapp.com/GDdd0I3WlF5LnMVifmS7rN' ,MessageType.text);
+}
+else if (text == 'Euy'){
+conn.sendMessage(id, 'Ya?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'euy'){
+conn.sendMessage(id, 'Ya?, Ketik .help/.info/.donasi Contoh .help' ,MessageType.text);
+}
+else if (text == 'makasi'){
+conn.sendMessage(id, 'Sama sama, semoga harimu menyenangkan :)' ,MessageType.text);
+}
+else if (text == 'Makasi'){
+conn.sendMessage(id, 'Sama sama, semoga harimu menyenangkan :)' ,MessageType.text);
+}
+else if (text == 'makasih'){
+conn.sendMessage(id, 'Sama sama, semoga harimu menyenangkan :)' ,MessageType.text);
+}
+else if (text == 'Makasih'){
+conn.sendMessage(id, 'Sama sama, semoga harimu menyenangkan :)' ,MessageType.text);
+}
+else if (text == 'thank'){
+conn.sendMessage(id, 'Sama sama, semoga harimu menyenangkan :)' ,MessageType.text);
+}
+else if (text == 'Thank'){
+conn.sendMessage(id, 'Sama sama, semoga harimu menyenangkan :)' ,MessageType.text);
+}
+else if (text == 'thanks'){
+conn.sendMessage(id, 'Sama sama, semoga harimu menyenangkan :)' ,MessageType.text);
+}
+else if (text == 'Thanks'){
+conn.sendMessage(id, 'Sama sama, semoga harimu menyenangkan :)' ,MessageType.text);
+}
 
 const
 {
